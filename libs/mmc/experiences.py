732dd104e3cca34e643aa410cdd623232b0006c5
Emmanuel Acosta <eacosta@roundabout.com>
2023-02-07 15:41:27 -0800

    Season Experience complete

--- libs/mmc/experiences.py
+++ libs/mmc/experiences.py
@@ -7,18 +7,25 @@ if TYPE_CHECKING:
     from xml.etree import ElementTree as ET
 
     from .inventory import Metadata
+    from .mmc_core import Season, Episode
     from .presentations import EpPresentation
 
 class Experience(ABC):
+    def __init__(self, metadata: "Metadata") -> None:
+        self.metadata = metadata
+        self.id = self._exp_id()
+        self.rootelem = newelement("manifest", "Experience")
+        
     @abstractmethod
     def generate(self) -> "ET.Element":...
+
+    @abstractmethod
+    def _exp_id(self) -> str:...
 
 class EpisodeExperience(Experience):
     def __init__(self, presentation: "EpPresentation", metadata: "Metadata") -> None:
         self.presentation = presentation
-        self.metadata = metadata
-        self.id = self._exp_id()
-        self.rootelem = newelement("manifest", "Experience")
+        super().__init__(metadata)
 
     def generate(self) -> "ET.Element":
         self.rootelem.set("ExperienceID", self.id)
@@ -27,7 +34,7 @@ class EpisodeExperience(Experience):
         self.rootelem.append(str_to_element("manifest", "ContentID", self.metadata.id))
 
         av_root = newelement("manifest", "Audiovisual")
-        av_root.set("ContentID", self._av_id())
+        av_root.set("ContentID", self._exp_id(is_av=True))
         av_root.append(str_to_element("manifest", "Type", "Main"))
         av_root.append(str_to_element("manifest", "SubType", "Episode"))
         av_root.append(str_to_element("manifest", "PresentationID", self.presentation.id))
@@ -42,23 +49,48 @@ class EpisodeExperience(Experience):
         self.rootelem.append(expchild_root)
         return self.rootelem
     
-    def _exp_id(self) -> str:
+    def _exp_id(self, is_av: bool=False) -> str:
         # "md:experienceid:org:amazonkids:HELLO_KITTY_INTL_S1_106:episode.6.en"
         org = self.presentation.mec.org
         id = self.presentation.mec.id
         seq = self.presentation.seq
         language = self.presentation.language
-        return f"md:experienceid:org:{org}:{id}:episode.{seq}.{language}"
-
-    def _av_id(self) -> str:
-        # md:cid:org:amazonkids:HELLO_KITTY_INTL_S1_106:av.episode.6
-        org = self.presentation.mec.org
-        id = self.presentation.mec.id
-        seq = self.presentation.seq
-        return f"md:cid:org:{org}:{id}:av.episode.{seq}"
+        av = "av." if is_av else ""
+        return f"md:experienceid:org:{org}:{id}:{av}episode.{seq}.{language}"
 
 class SeasonExperience(Experience):
-    pass
+    def __init__(self, season: "Season", language: str) -> None:
+        self.season = season
+        self.language = language
+        super().__init__(season.metadata)
+
+    def generate(self) -> "ET.Element":
+        self.rootelem.set("ExperienceID", self.id)
+        self.rootelem.append(str_to_element("manifest", "ContentID", self.metadata.id))
+
+        for ep in self.season.episodes:
+            expchild_root = newelement("manifest", "ExperienceChild")
+            expchild_root.append(str_to_element("manifest", "Relationship", "isepisodeof"))
+
+            seq_root = newelement("manifest", "SequenceInfo")
+            seq_root.append(str_to_element("md", "Number", ep.seq))
+            expchild_root.append(seq_root)
+
+            try:
+                ep_exp_id = ep.experiences[self.language].id
+            except KeyError:
+                raise RuntimeError(f"Unable to locate language '{self.language}' for episode '{ep.mec.id}'")
+            expchild_root.append(str_to_element("manifest", "ExperienceID", ep_exp_id))
+            self.rootelem.append(expchild_root)
+            
+        return self.rootelem
+
+    def _exp_id(self) -> str:
+        # "md:experienceid:org:amazonkids:HELLO_KITTY_INTL_S1_106:season.6.en"
+        org = self.season.mec.org
+        id = self.season.mec.id
+        seq = self.season.seq
+        return f"md:experienceid:org:{org}:{id}:season.{seq}.{self.language}"
 
 class SeriesExperience(Experience):
     pass
--- libs/mmc/mmc_core.py
+++ libs/mmc/mmc_core.py
@@ -1,6 +1,6 @@
 from abc import ABC
 from pathlib import Path
-from typing import Any, TYPE_CHECKING, cast
+from typing import TYPE_CHECKING, cast
 
 from .. import errors
 from ..mec import MECEpisodic
@@ -8,8 +8,8 @@ from ..enums import WorkTypes
 from ..xmlhelpers import newroot, newelement, str_to_element
 
 from .presentations import EpPresentation
-from .experiences import EpisodeExperience
 from .inventory import Audio, Video, Subtitle, Metadata
+from .experiences import EpisodeExperience, SeasonExperience
 
 if TYPE_CHECKING:
     from ..mec import MEC, MECGroup
@@ -36,17 +36,17 @@ class Episode(MMCEntity):
         super().__init__(mec, ext, checksums)
         self.seq = self.mec.search_media("SequenceInfo", assertcurrent=True)
         self._parse_resources()
-        self._presentations: list[EpPresentation] = []
-        self._experiences: list[EpisodeExperience] = []
+        self._presentations: dict[str, EpPresentation] = {}
+        self._experiences: dict[str, EpisodeExperience] = {}
 
     @property
-    def presentations(self) -> list[EpPresentation]:
+    def presentations(self) -> dict[str, EpPresentation]:
         if not self._presentations:
             self._presentations = self._gen_presentations()
         return self._presentations
 
     @property
-    def experiences(self) -> list[EpisodeExperience]:
+    def experiences(self) -> dict[str, EpisodeExperience]:
         if not self._experiences:
             self._experiences = self._gen_experiences()
         return self._experiences
@@ -59,7 +59,7 @@ class Episode(MMCEntity):
             elif res.fullpath.suffix.lower() in self.extensions.sub_exts:
                 self.subtitle.append(Subtitle(self.mec, self.checksums, res))
 
-    def _gen_presentations(self) -> list[EpPresentation]:
+    def _gen_presentations(self) -> dict[str, EpPresentation]:
         """
         Can be extended to support custom groupings of video/audio/sub languages.
         For now, it has to have exactly 1 video/audio/sub in 1 language.
@@ -72,16 +72,41 @@ class Episode(MMCEntity):
             len(self.audio) != 1 or
             len(self.subtitle) != 1):
             raise NotImplementedError(f"Currently only supports exactly 1 video/audio/subtitle file")
-        return [EpPresentation(self.mec, self.audio[0], self.video[0], self.subtitle[0], self.video[0].language)]
+        pres_perlanguage: dict[str, EpPresentation] = {}
+        try:
+            sub = self.subtitle[0]
+        except KeyError:
+            sub = None
+        pres = EpPresentation(self.mec, self.audio[0], self.video[0], sub, self.video[0].language)
+        pres_perlanguage[self.video[0].language] = pres
+        return pres_perlanguage
 
-    def _gen_experiences(self) -> list[EpisodeExperience]:
-        return [EpisodeExperience(pres, self.metadata) for pres in self.presentations]
+    def _gen_experiences(self) -> dict[str, EpisodeExperience]:
+        exps: dict[str, EpisodeExperience] = {}
+        for k,v in self.presentations.items():
+            exps[k] = EpisodeExperience(v, self.metadata)
+        return exps
 
 class Season(MMCEntity):
     def __init__(self, mec: "MEC", episodes: list["MEC"], ext: Extensions, checksums: list[str]) -> None:
         super().__init__(mec, ext, checksums)
         self.episodes = [Episode(ep, ext, checksums) for ep in episodes]
         self.seq = self.mec.search_media("SequenceInfo", assertcurrent=True)
+        self._experiences: list[SeasonExperience] = []
+
+    @property
+    def experiences(self) -> list[SeasonExperience]:
+        if not self._experiences:
+            self._experiences = self._gen_experiences()
+        return self._experiences
+
+    def _gen_experiences(self) -> list[SeasonExperience]:
+        if len(self.episodes) < 1:
+            raise RuntimeError(f"Season '{self.mec.id}' has no Episodes")
+        exps: list[SeasonExperience] = []
+        for lang in self.episodes[0].presentations.keys():
+            exps.append(SeasonExperience(self, lang))
+        return exps
 
 class Series(MMCEntity):
     def __init__(self, rootdir: Path, mecgroup: "MECEpisodic") -> None:
@@ -109,7 +134,7 @@ class Series(MMCEntity):
         presentations_root = newelement("manifest", "Presentations")
         for season in self.seasons:
             for ep in season.episodes:
-                for pres in ep.presentations:
+                for pres in ep.presentations.values():
                     presentations_root.append(pres.generate())
         return presentations_root
 
@@ -117,8 +142,10 @@ class Series(MMCEntity):
         exp_root = newelement("manifest", "Experiences")
         for season in self.seasons:
             for ep in season.episodes:
-                for exp in ep.experiences:
+                for exp in ep.experiences.values():
                     exp_root.append(exp.generate())
+            for exp in season.experiences:
+                exp_root.append(exp.generate())
         return exp_root
 
     def _readmd5(self) -> list[str]:
