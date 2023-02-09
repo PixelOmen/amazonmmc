482f6caff8862efac8132266175770fd255824d9
Emmanuel Acosta <eacosta@roundabout.com>
2023-02-08 16:59:04 -0800

    Moved to install setup.py structure

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='amazonmmc',
+    version='0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={"src": ["samples"]},
+    entry_points={
+        'console_scripts': [
+            'amazonmmc = src:main',
+        ]
+    }
+)
