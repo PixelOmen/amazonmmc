a876755df1c54d490b9963f80d7f94634aaa9e26
Emmanuel Acosta <eacosta@roundabout.com>
2023-02-09 13:28:11 -0800

    Updated setup.py with amazonmmc dir

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.1',
     packages=find_packages(),
     include_package_data=True,
-    package_data={"src": ["samples"]},
+    package_data={"amazonmmc": ["samples"]},
     entry_points={
         'console_scripts': [
             'amazonmmc = src:main',
