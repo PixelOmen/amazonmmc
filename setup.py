b58a90212d080ae75e8b47e7a676aa013104956a
Emmanuel Acosta <eacosta@roundabout.com>
2023-02-09 14:21:30 -0800

    Slightly changed syntax for samples in package data in setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='amazonmmc',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(),
     include_package_data=True,
-    package_data={"amazonmmc": ["samples"]},
+    package_data={"amazonmmc": ["samples/*"]},
     entry_points={
         'console_scripts': [
             'amazonmmc = amazonmmc:main',
