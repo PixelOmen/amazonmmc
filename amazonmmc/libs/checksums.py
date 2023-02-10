37ee95424177a5142f8eea414ddd8309a082586b
Emmanuel Acosta <eacosta@roundabout.com>
2023-02-10 12:16:01 -0800

    Added verbose option to MD5.run

--- amazonmmc/libs/checksums.py
+++ amazonmmc/libs/checksums.py
@@ -7,7 +7,7 @@ class MD5:
     def __init__(self, rootdir: Path) -> None:
         self.rootdir = rootdir
 
-    def run(self) -> dict[str, str]:
+    def run(self, verbose: bool=True) -> dict[str, str]:
         if sys.platform != "darwin":
             raise OSError("Checksums currently only supported on MacOS")
         resourcedir = self.rootdir / "resources"
@@ -18,7 +18,7 @@ class MD5:
             files.append(item)
         output: list[tuple[str, str]] = []
         with futures.ThreadPoolExecutor() as executor:
-            checksums = [executor.submit(self._runprocess, file) for file in files]
+            checksums = [executor.submit(self._runprocess, file, verbose) for file in files]
             for md5 in futures.as_completed(checksums):
                 output.append(md5.result())
         hashdict: dict[str, str] = {}
@@ -27,10 +27,14 @@ class MD5:
             hashdict[path] = hash
         return hashdict
 
-    def _runprocess(self, file: Path) -> tuple[str, str]:
+    def _runprocess(self, file: Path, verbose: bool=True) -> tuple[str, str]:
+        if verbose:
+            print(f"Running checksum: {file.name}...")
         proc = sub.run(f"md5 -r {str(file)}", stdout=sub.PIPE, stderr=sub.PIPE, shell=True)
         stdout = proc.stdout.decode("UTF-8").replace("\r\n", " ")
         stderr = proc.stderr.decode("UTF-8").replace("\r\n", " ")
         if stderr:
             raise RuntimeError(f"{file.name}: {stderr}")
+        if verbose:
+            print(f"Checksum complete: {file.name}")
         return file.name, stdout
