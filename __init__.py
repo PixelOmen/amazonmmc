3a6e9313e284122c9383333cf09ff005c12f4bf5
Emmanuel Acosta <eacosta@roundabout.com>
2023-03-22 14:39:02 -0700

    Made more submodule friendly

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from .amazonmmc import *
--- amazonmmc/__init__.py
+++ amazonmmc/__init__.py
@@ -1,1 +1,4 @@
 from .entrypoint import main
+from .libs import enums, errors
+from .libs.delivery import Delivery
+from .libs.args import MMCArgs, parse_args
